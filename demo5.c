53b9b652b7c5d4d55410ed2207fd0f82081ef2e7
ishu sharma <ishu83543@gmail.com>
2025-07-26 15:14:26 +0530

    q = 6  , ans = 6

---
+++ demo5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void main()
+{
+	int a = 2, b=3, c=4, d= 5,e =6;
+	int res1 = a*a;
+	int res2 = b*b;
+	int res3 = c*c;
+	int res4 = d*d;
+	int res5 = e*e;
+	
+	printf("a=%d" ,res1);
+	printf("b=%d" ,res2);
+	printf("c=%d" ,res3);
+	printf("d=%d" ,res4);
+	printf("e=%d" ,res5);
+	
+	int res6 = a*a*a;
+	int res7 = b*b*b;
+	int res8 = c*c*c;
+	int res9 = d*d*d;
+	int res10 = e*e*e;
+	
+	printf("a=%d\n" , res6);
+	printf("b=%d" , res7);
+	printf("c=%d" , res8);
+	printf("d=%d" , res9);
+	printf("e=%d" , res10);
+	
+	
+	
+}
